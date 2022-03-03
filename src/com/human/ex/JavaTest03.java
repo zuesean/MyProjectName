e99c6b90136274e12e88d2e1883dc9cb9dbb1141
admin <admin@DESKTOP-1TBMU57>
2022-03-03 14:27:13 +0900

    first commit

---
+++ src/com/human/ex/JavaTest03.java
@@ -0,0 +1,73 @@
+package com.human.ex;
+
+public class JavaTest03 {
+	public static void main(String[] args) {
+		System.out.println
+		(
+				"Hello World"
+				)
+		;
+		
+		System.out.println("안녕자바");
+		System.out.println("안성훈");
+		
+		System.out.print("\n");
+		
+		System.out.println("*****");
+		System.out.println("*****");
+		System.out.println("*****");
+		System.out.println("*****");
+		System.out.println("*****");
+		
+		System.out.print("\n");
+		
+		System.out.print("    ");
+		System.out.print("*");
+		System.out.print("\n");
+		System.out.print("   ");
+		System.out.print("**");
+		System.out.print("\n");
+		System.out.print("  ");
+		System.out.print("***");
+		System.out.print("\n");
+		System.out.print(" ");
+		System.out.print("****");
+		System.out.print("\n");
+		System.out.println("*****");
+		
+		System.out.print("\n");
+		
+		System.out.print("*");
+		System.out.print("\n");
+		System.out.print("**");
+		System.out.print("\n");
+		System.out.print("***");
+		System.out.print("\n");
+		System.out.print("****");
+		System.out.print("\n");
+		System.out.println("*****");
+		
+		System.out.print("\n");
+		
+		System.out.println("******************");
+		System.out.println("* 국어 50         *");
+		System.out.println("******************");
+		System.out.println("* 국어 50         *");
+		System.out.println("******************");
+		System.out.println("* 국어 50         *");
+		System.out.println("******************");
+		System.out.println("* 컴퓨터학과 홍길동  *");
+		System.out.println("******************");
+		
+		System.out.print("\n");
+		
+		System.out.println("일\t 월\t 화\t 수\t 목\t 금\t 토\t");
+		System.out.println("2.27\t2.28\t 1\t 2\t 3\t 4\t 5\t");
+		System.out.println("6\t 7\t 8\t 9\t 10\t 11\t 12\t");
+		System.out.println("13\t 14\t 15\t 16\t 17\t 18\t 19\t");
+		System.out.println("20\t 21\t 22\t 23\t 24\t 25\t 26\t");
+		System.out.println("27\t 28\t 29\t 30\t 31\t");
+						
+	}
+
+}
---
+++ src/com/human/ex/javaTest02.java
@@ -0,0 +1,21 @@
+package com.human.ex;
+
+public class javaTest02 {
+
+	public static void main(String[] args ) {
+		System.out.println("hello");
+		System.out.println("World");
+		System.out.println("hello\tWorld");
+		System.out.println("hello\nWorld");
+		
+		System.out.print("hello");
+		System.out.print("World");
+		System.out.print("\n");
+		
+		System.out.print("hello\"World\"");
+		
+		// "\" = 이스케이프 시퀀스. 양쪽에 붙임으로써 문법 건너뛰기 실행.
+		
+
+	}
+}
