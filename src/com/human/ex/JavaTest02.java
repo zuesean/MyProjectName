bc139ec0358d9e0a8de5f2215402cf1d469047c6
admin <admin@DESKTOP-1TBMU57>
2022-03-03 14:58:00 +0900

    First Commit

--- src/com/human/ex/JavaTest02.java
+++ src/com/human/ex/JavaTest02.java
@@ -1,6 +1,6 @@
 package com.human.ex;
 
-public class JavaTest03 {
+public class JavaTest02 {
 	public static void main(String[] args) {
 		System.out.println
 		(
--- src/com/human/ex/javaTest01.java
+++ src/com/human/ex/javaTest01.java
@@ -1,7 +1,22 @@
 package com.human.ex;
 
-public class javaTest02 {
+public class javaTest01 {
 
+	/*
+	 * 여러 줄 주석이라고 함.
+	 * 여러 줄 써도 상관없어서 버그 안남.
+	 * 열면 반드시 닫을 것.
+	 * 단축키 일람
+	 * 컨트롤 + Home = 커서 기준으로 공백 처음으로 영역선택 이동
+	 * 컨트롤 + End = 커서 기준으로 공백 마지막까지 영역선택 이동
+	 * Home, End = 영역 선택하지 않고 커서 기준으로 처음 및 끝으로 이동
+	 * 컨트롤 + Shift + 방향키 = 단어 기준으로 이동하면서 영역 선택
+	 * 컨트롤 + F = 모두 바꾸기
+	 * 컨트롤 + Z = 실행 취소
+	 * 컨트롤 + Y = 다시 실행
+	 * 컨트롤 + C,V,X = 복, 붙, 짤
+	 */
+	
 	public static void main(String[] args ) {
 		System.out.println("hello");
 		System.out.println("World");
